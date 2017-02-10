d9c79458d09286bf278be77145fddf62dabee442
zxqc <pengyun.li@etjourney.com>
2017-02-10 13:24:48 +0800

    Initial Commit

---
+++ ObjectiveTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveTableView
+//
+//  Created by macPro on 2017/2/10.
+//  Copyright © 2017年 macPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveTableView
+//
+//  Created by macPro on 2017/2/10.
+//  Copyright © 2017年 macPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
